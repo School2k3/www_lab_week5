06f0ce95e44f0114b8c2645a8bad78f79f01d9d9
School2k3 <truong29042003@gmail.com>
2024-11-29 21:16:47 +0700

    add enums

---
+++ src/main/java/vn/edu/iuh/fit/backend/enums/SkillLevel.java
@@ -0,0 +1,11 @@
+package vn.edu.iuh.fit.backend.enums;
+
+public enum SkillLevel {
+    BEGINNER(1),
+    INTERMEDIATE(2),
+    ADVANCED(3),
+    PROFESSIONAL(4),
+    MASTER(5);
+    SkillLevel(int value) {
+    }
+}
---
+++ src/main/java/vn/edu/iuh/fit/backend/enums/SkillType.java
@@ -0,0 +1,9 @@
+package vn.edu.iuh.fit.backend.enums;
+
+public enum SkillType {
+    UNSPECIFIC(0),
+    TECHNICAL_SKILL(1),
+    SOFT_SKILL(2);
+    SkillType(int value) {
+    }
+}
